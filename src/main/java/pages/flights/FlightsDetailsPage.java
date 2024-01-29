46fe3930f3e20a3abb99bd688d335fa7c6d67108
adakayaaa <ummuhaneltn@gmail.com>
2024-01-29 10:45:07 +0100

    updated flights details page and tested

--- src/main/java/pages/flights/FlightsDetailsPage.java
+++ src/main/java/pages/flights/FlightsDetailsPage.java
@@ -1,10 +1,11 @@
 package pages.flights;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import pages.BasePage;
+import utils.BrowserUtils;
 
-import javax.xml.xpath.XPath;
 import java.util.List;
 
 public class FlightsDetailsPage extends BasePage {
@@ -23,23 +24,110 @@ public class FlightsDetailsPage extends BasePage {
 
     @FindBy(xpath = "//h1[@class=\"flight-title\"]/../span[@class=\"text-muted fs-5\"]")
     private List<WebElement> tripTypes;
-    public String getTitleOfDetailsPage(){
+    @FindBy(css = ".listResultTabs>button")
+    private List<WebElement> cheapestAndFastestAndAllButtons;
+    @FindBy(css = ".siDetailTexts>.siPrice")
+    private List<WebElement> flightPrices;
+    @FindBy(xpath = "//div[@class='flight-duration']/span[1]")
+    private List<WebElement> flightTimes;
+    @FindBy(xpath = "(//div[@class='lsOptions p-0'])[1]/div/input")
+    private List<WebElement> flightCabinTypes;
+    @FindBy(xpath = "//div[@class='searchItem flightItem']//span[@class='siCancelOpSubtitle']")
+    private List<WebElement> cabinClassOnTickets;
+
+    public String getTitleOfDetailsPage() {
         return titleOfDetailsPage.getText();
     }
 
-    public boolean isSelectedDepartureCountry(String departureCountry){
+    public boolean isSelectedDepartureCountry(String departureCountry) {
+        List<String> list = departureCountries.stream().map(WebElement::getText).toList();
+        System.out.println(list);
         return departureCountries.stream().allMatch(country -> country.getText().contains(departureCountry));
     }
-    public boolean isSelectedArrivalCountry(String arrivalCountry){
+
+    public boolean isSelectedArrivalCountry(String arrivalCountry) {
+        List<String> list = arrivalCountries.stream().map(WebElement::getText).toList();
+        System.out.println(list);
         return arrivalCountries.stream().allMatch(country -> country.getText().contains(arrivalCountry));
     }
 
-    public boolean isDepartureDate(String departureDay){
-        return departureDates.stream().allMatch(day ->day.getText().contains(departureDay));
+    public boolean isDepartureDate(String departureDay) {
+        return departureDates.stream().allMatch(day -> day.getText().contains(departureDay));
     }
 
-    public boolean isRoundType(){
+    public boolean isRoundType() {
         return tripTypes.stream().allMatch(trip -> trip.getText().contains("Round Trip"));
+    }
+
+    public void clickOnCheapestTab() {
+        cheapestAndFastestAndAllButtons.get(0).click();
+    }
+
+    public void clickOnFastestTab() {
+        cheapestAndFastestAndAllButtons.get(1).click();
+    }
+
+    public void clickOnAllTab() {
+        cheapestAndFastestAndAllButtons.get(2).click();
+    }
+
+    public boolean isSortedFromCheapestToHighest() {
+        List<Integer> prices = flightPrices.stream().map(price -> Integer.parseInt(price.getText().substring(1))).toList();
+        for (int i = 0; i < prices.size() - 1; i++) {
+            if (prices.get(i) > prices.get(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isSortableFromFastestToSlowest() {
+        List<Double> listOfHours = flightTimes.stream().map(hour -> Double.parseDouble(hour.getText().substring(0, hour.getText().indexOf(" ")))).toList();
+        for (int i = 0; i < listOfHours.size() - 1; i++) {
+            if (listOfHours.get(i) > listOfHours.get(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void clickOnAllCabinTypes() {
+        BrowserUtils.scrollDownWithPageDown();
+        flightCabinTypes.get(0).click();
+        flightCabinTypes.get(1).click();
+        flightCabinTypes.get(2).click();
+    }
+
+    public boolean isFirstClassCabin(String classType) {
+        flightCabinTypes.get(0).click();
+        BrowserUtils.scrollUpWithPageUp();
+        if (cabinClassOnTickets.isEmpty()) {
+            return true;
+        } else {
+            return cabinClassOnTickets.stream().allMatch(ticket -> ticket.getText().contains(classType));
+        }
+    }
+
+    public boolean isEconomyCabin(String classType) {
+        flightCabinTypes.get(1).click();
+        BrowserUtils.scrollUpWithPageUp();
+
+        if (cabinClassOnTickets.isEmpty()) {
+            return true;
+        } else {
+            return cabinClassOnTickets.stream().allMatch(ticket -> ticket.getText().contains(classType));
+        }
+    }
+
+    public boolean isBusinessCabin(String classType) {
+        flightCabinTypes.get(2).click();
+        BrowserUtils.scrollUpWithPageUp();
+
+        if (cabinClassOnTickets.isEmpty()) {
+            return true;
+        } else {
+            return cabinClassOnTickets.stream().allMatch(ticket -> ticket.getText().contains(classType));
+        }
     }
 
 }
--- src/test/java/step_defs/flights/FlightsDetailsPage.java
+++ src/test/java/step_defs/flights/FlightsDetailsPage.java
@@ -1,6 +1,46 @@
 package step_defs.flights;
 
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import step_defs.BaseStep;
 
+
+import static org.assertj.core.api.BDDAssertions.then;
+
 public class FlightsDetailsPage extends BaseStep {
+    @When("The user clicks on the cheapest tab")
+    public void theUserClicksOnTheCheapestTab() {
+        PAGES.getFlightsDetailsPage().clickOnCheapestTab();
+    }
+
+    @Then("The user should see that fees of tickets are arranged from cheapest to highest")
+    public void theUserShouldSeeThatFeesOfTicketsAreArrangedFromCheapestToHighest() {
+        then(PAGES.getFlightsDetailsPage().isSortedFromCheapestToHighest()).isTrue().withFailMessage("Prices are not sorted!");
+
+    }
+
+    @When("The user click on fastest tab")
+    public void theUserClickOnFastestTab() {
+        PAGES.getFlightsDetailsPage().clickOnFastestTab();
+
+    }
+
+
+    @Then("The user should see that times of flights are arranged from fastest to slowest")
+    public void theUserShouldSeeThatTimesOfFlightsAreArrangedFromFastestToSlowest() {
+        then(PAGES.getFlightsDetailsPage().isSortableFromFastestToSlowest()).isTrue().withFailMessage("Times are not sorted!");
+
+    }
+
+
+    @When("The user selects from checkbox {string} in the cabin class")
+    public void theUserSelectsFromCheckboxInTheCabinClass(String classType) {
+        PAGES.getFlightsDetailsPage().clickOnAllCabinTypes();
+    }
+
+    @Then("The user should see only {string} on the tickets")
+    public void theUserShouldSeeOnlyOnTheTickets(String classType) {
+        then(PAGES.getFlightsDetailsPage().isEconomyCabin(classType)).isTrue().withFailMessage("The ticket type is not as expected!");
+
+    }
 }
