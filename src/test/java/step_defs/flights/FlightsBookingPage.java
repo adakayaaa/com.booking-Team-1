22e11bec6b66260f89bfae5e2f7f7a4b8e6eeba7
yusuf <yusufemiruyanik.gmail.com>
2024-01-31 13:39:12 +0300

    T1-125-neagative scenarios added and tested

--- src/test/java/step_defs/flights/FlightsBookingPage.java
+++ src/test/java/step_defs/flights/FlightsBookingPage.java
@@ -5,62 +5,76 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import step_defs.BaseStep;
+import utils.BrowserUtils;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
 public class FlightsBookingPage extends BaseStep {
 
-	private static final Logger LOGGER = LogManager.getLogger(FlightsBookingPage.class);
+    private static final Logger LOGGER = LogManager.getLogger(FlightsBookingPage.class);
 
-	@Then("The user should see that {string} text")
-	public void theUserShouldSeeThatText(String titleOfBookingPage) {
-		then(PAGES.getFlightsBookingPage().getTitleOfBookingPage()).isEqualTo(titleOfBookingPage)
-			.withFailMessage("The select who's flying button is not working on the result page");
+    @Then("The user should see that {string} text")
+    public void theUserShouldSeeThatText(String titleOfBookingPage) {
+        then(PAGES.getFlightsBookingPage().getTitleOfBookingPage()).isEqualTo(titleOfBookingPage)
+                .withFailMessage("The select who's flying button is not working on the result page");
 
-	}
+    }
 
-	@And("The user should see that only {string} and {string} are displayed")
-	public void theUserShouldSeeThatOnlyAndAreDisplayed(String fromDepartureToArrival, String fromArrivalToDeparture) {
-		then(PAGES.getFlightsBookingPage().getFromDepartureCountryToArrivalCountry()).isEqualTo(fromDepartureToArrival);
-		then(PAGES.getFlightsBookingPage().getFromArrivalCountryToDepartureCountry()).isEqualTo(fromArrivalToDeparture);
-	}
+    @And("The user should see that only {string} and {string} are displayed")
+    public void theUserShouldSeeThatOnlyAndAreDisplayed(String fromDepartureToArrival, String fromArrivalToDeparture) {
+        then(PAGES.getFlightsBookingPage().getFromDepartureCountryToArrivalCountry()).isEqualTo(fromDepartureToArrival);
+        then(PAGES.getFlightsBookingPage().getFromArrivalCountryToDepartureCountry()).isEqualTo(fromArrivalToDeparture);
+    }
 
-	@And("The user should see that only {string} on the tickets")
-	public void theUserShouldSeeThatOnlyOnTheTickets(String tripType) {
-		then(PAGES.getFlightsBookingPage().getTextOfTripType()).isEqualTo(tripType)
-			.withFailMessage("The trip type is wrong!");
-	}
+    @And("The user should see that only {string} on the tickets")
+    public void theUserShouldSeeThatOnlyOnTheTickets(String tripType) {
+        then(PAGES.getFlightsBookingPage().getTextOfTripType()).isEqualTo(tripType)
+                .withFailMessage("The trip type is wrong!");
+    }
 
-	@And("The user should see that taxes are calculated truly")
-	public void theUserShouldSeeThatTaxesAreCalculatedTruly() {
-		then(PAGES.getFlightsBookingPage().isEqualToTotalPrice()).isTrue().withFailMessage("The taxes are not correct");
-	}
+    @And("The user should see that taxes are calculated truly")
+    public void theUserShouldSeeThatTaxesAreCalculatedTruly() {
+        then(PAGES.getFlightsBookingPage().isEqualToTotalPrice()).isTrue().withFailMessage("The taxes are not correct");
+    }
 
-	@When("The user enters  as {string} email and  as {string} phone number")
-	public void theUserEntersAsEmailAndAsPhoneNumber(String email, String phoneNumber) {
-		PAGES.getFlightsBookingPage().enterEmail(email);
-		PAGES.getFlightsBookingPage().enterPhoneNumber(phoneNumber);
-	}
+    @When("The user enters  as {string} email and  as {string} phone number")
+    public void theUserEntersAsEmailAndAsPhoneNumber(String email, String phoneNumber) {
+        PAGES.getFlightsBookingPage().enterEmail(email);
+        PAGES.getFlightsBookingPage().enterPhoneNumber(phoneNumber);
+    }
 
-	@And("The user enters as {string} first name and as {string} last name")
-	public void theUserEntersAsFirstNameAndAsLastName(String firstName, String lastName) {
-		PAGES.getFlightsBookingPage().enterFirstName(firstName);
-		PAGES.getFlightsBookingPage().enterLastName(lastName);
-	}
+    @And("The user enters as {string} first name and as {string} last name")
+    public void theUserEntersAsFirstNameAndAsLastName(String firstName, String lastName) {
+        PAGES.getFlightsBookingPage().enterFirstName(firstName);
+        PAGES.getFlightsBookingPage().enterLastName(lastName);
+    }
 
-	@And("The user select gender as {string} and birthdate as {string} year and {string} month and {string} day")
-	public void theUserSelectGenderAsAndBirthdateAsYearAndMonthAndDay(String gender, String year, String month,
-			String day) {
-		PAGES.getFlightsBookingPage().selectGender(gender);
-		PAGES.getFlightsBookingPage().selectBirthDate(year, month, day);
-	}
+    @And("The user select gender as {string} and birthdate as {string} year and {string} month and {string} day")
+    public void theUserSelectGenderAsAndBirthdateAsYearAndMonthAndDay(String gender, String year, String month,
+                                                                      String day) {
+        PAGES.getFlightsBookingPage().selectGender(gender);
+        PAGES.getFlightsBookingPage().selectBirthDate(year, month, day);
+    }
 
-	@And("The user clicks on select extras button")
-	public void theUserClicksOnSelectExtrasButton() {
-		PAGES.getFlightsBookingPage().clickOnExtraButton();
-	}
+    @And("The user clicks on select extras button")
+    public void theUserClicksOnSelectExtrasButton() {
+        PAGES.getFlightsBookingPage().clickOnExtraButton();
+    }
 
+    @When("The user enters  as invalid {string} email and  as invalid {string} phone number")
+    public void theUserEntersAsInvalidEmailAndAsInvalidPhoneNumber(String email, String phoneNumber) {
+        PAGES.getFlightsBookingPage().enterEmail(email);
+        PAGES.getFlightsBookingPage().enterPhoneNumber(phoneNumber);
+    }
+
+    @Then("The user should see  alert message")
+    public void theUserShouldSeeAlertMessage() {
+        Alert alert = DRIVER.switchTo().alert();
+        then(alert.getText()).withFailMessage("The user shouldn't click select extra button").isEqualTo("Please fill the following fields:");
+    }
 }
+
