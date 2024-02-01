fbe78b161612391219cd80f7bea2a597f4c78c62
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2024-02-01 17:34:13 +0300

    last changes have been made

--- src/test/java/step_defs/car_rentals/CarRentalHomeStep.java
+++ src/test/java/step_defs/car_rentals/CarRentalHomeStep.java
@@ -7,6 +7,7 @@ import io.cucumber.java.en.When;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.assertj.core.api.Assertions;
+import org.openqa.selenium.NoAlertPresentException;
 import pages.HomePage;
 import step_defs.BaseStep;
 import utils.BrowserUtils;
@@ -15,64 +16,85 @@ import static org.assertj.core.api.BDDAssertions.then;
 
 public class CarRentalHomeStep extends BaseStep {
 
-	private static final Logger LOGGER = LogManager.getLogger(CarRentalHomeStep.class);
+    private static final Logger LOGGER = LogManager.getLogger(CarRentalHomeStep.class);
 
-	HomePage homePage;
+    HomePage homePage;
 
-	@Given("The user navigates to the Inar Academy Home page")
-	public void the_user_navigates_to_the_Inar_Academy_Home_page() {
-		homePage = PAGES.getHomePage();
-	}
+    @Given("The user navigates to the Inar Academy Home page")
+    public void the_user_navigates_to_the_Inar_Academy_Home_page() {
 
-	@When("The user clicks on the Booking link")
-	public void the_user_clicks_on_the_Booking_link() {
-		homePage.clickOnBookingLink();
-	}
+        homePage = PAGES.getHomePage();
+        LOGGER.info("The user navigates to the Inar Academy Home page");
+    }
 
-	@Then("The user sees Booking Home page")
-	public void the_user_sees_Booking_Home_page() {
-		String actualHeader = PAGES.getCarRentalsHomePage().getInarBookingHeaderText();
-		String expectedHeader = "inarbooking";
-		Assertions.assertThat(expectedHeader)
-			.withFailMessage("The user is not on the Booking" + " home page")
-			.isEqualTo(actualHeader);
-	}
+    @When("The user clicks on the Booking link")
+    public void the_user_clicks_on_the_Booking_link() {
 
-	@And("The user clicks on the Car rentals tab")
-	public void the_user_clicks_on_the_Car_rentals_tab() {
-		BrowserUtils.wait(1.0);
-		PAGES.getCarRentalsHomePage().clickOnTheCarRentalsLink();
-	}
+        homePage.clickOnBookingLink();
+        LOGGER.info("The user clicks on the Booking link");
+    }
 
-	@Then("The user validates that {string} message is visible")
-	public void the_user_validates_that_true_message_is_visible(String expectedMessage) {
-		String actualMessage = PAGES.getCarRentalsHomePage().getHeaderTitleMessage();
-		Assertions.assertThat(actualMessage)
-			.withFailMessage("The user is not on the " + "Booking home page")
-			.isEqualTo(expectedMessage);
-	}
+    @Then("The user sees Booking Home page")
+    public void the_user_sees_Booking_Home_page() {
+        String actualHeader = PAGES.getCarRentalsHomePage().getInarBookingHeaderText();
+        String expectedHeader = "inarbooking";
+        Assertions.assertThat(expectedHeader)
+                .withFailMessage("The user is not on the Booking home page")
+                .isEqualTo(actualHeader);
+        LOGGER.debug("The user sees Booking Home page");
+    }
 
-	@And("The user clicks on the search button")
-	public void the_user_clicks_on_the_search_button() {
-		PAGES.getCarRentalsHomePage().clickOnTheSearchButton();
-	}
+    @And("The user clicks on the Car rentals tab")
+    public void the_user_clicks_on_the_Car_rentals_tab() {
+        BrowserUtils.wait(1.0);
+        PAGES.getCarRentalsHomePage().clickOnTheCarRentalsLink();
+        LOGGER.info("The user clicks on the Car rentals tab");
+    }
 
-	@And("The user enter {string},{string},{string},{string} and {string}")
-	public void theUserEnterCredentialsInCarRentalsHomePage(String pickupLocation, String pickup_date,
-			String pickup_hour, String dropOffDate, String drop_hour) {
-		PAGES.getCarRentalsHomePage().enterThePickupLocation(pickupLocation);
-		PAGES.getCarRentalsHomePage().enterThePickUpDate(pickup_date);
-		PAGES.getCarRentalsHomePage().selectThePickupHour(pickup_hour);
-		PAGES.getCarRentalsHomePage().enterTheDropOffDate(dropOffDate);
-		PAGES.getCarRentalsHomePage().selectTheDropOffHour(drop_hour);
-	}
+    @Then("The user validates that {string} message is visible")
+    public void the_user_validates_that_true_message_is_visible(String expectedMessage) {
+        String actualMessage = PAGES.getCarRentalsHomePage().getHeaderTitleMessage();
+        Assertions.assertThat(actualMessage)
+                .withFailMessage("The user is not on the " + "Booking home page")
+                .isEqualTo(expectedMessage);
+        LOGGER.debug("The user validates that " + expectedMessage + " message is visible");
+    }
 
-	@Then("The user face with {string}")
-	public void theUserFaceWith(String errorMessage) {
-		String actualMessage = PAGES.getCarRentalsHomePage().getErrorMessageInHomePage();
-		then(actualMessage).isEqualTo(errorMessage);
-		LOGGER.debug("Error message displayed");
+    @And("The user clicks on the search button")
+    public void the_user_clicks_on_the_search_button() {
+        PAGES.getCarRentalsHomePage().clickOnTheSearchButton();
+        LOGGER.info("The user clicks on the search button");
+    }
 
-	}
+    @And("The user enter {string},{string},{string},{string} and {string}")
+    public void theUserEnterCredentialsInCarRentalsHomePage(String pickupLocation, String pickup_date,
+                                                            String pickup_hour, String dropOffDate, String drop_hour) {
+        PAGES.getCarRentalsHomePage().enterThePickupLocation(pickupLocation);
+        LOGGER.info("The user enter " + pickupLocation);
+
+        PAGES.getCarRentalsHomePage().enterThePickUpDate(pickup_date);
+        LOGGER.info("The user enter " + pickup_date);
+
+        PAGES.getCarRentalsHomePage().selectThePickupHour(pickup_hour);
+        LOGGER.info("The user enter " + pickup_hour);
+
+        PAGES.getCarRentalsHomePage().enterTheDropOffDate(dropOffDate);
+        LOGGER.info("The user enter " + dropOffDate);
+
+        PAGES.getCarRentalsHomePage().selectTheDropOffHour(drop_hour);
+        LOGGER.info("The user enter " + drop_hour);
+    }
+
+    @Then("The user face with {string}")
+    public void theUserFaceWith(String errorMessage) {
+        try {
+            String actualMessage = PAGES.getCarRentalsHomePage().getErrorMessageInHomePage();
+            then(actualMessage).isEqualTo(errorMessage);
+            LOGGER.debug("Error message displayed");
+        } catch (NoAlertPresentException ex) {
+            LOGGER.error(ex.getMessage());
+        }
+
+    }
 
 }
